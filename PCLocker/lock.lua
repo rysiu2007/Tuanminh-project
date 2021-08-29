cc1dee1f4614983f7dbd962a0bc51546f39f0ae2
Tuanminh batch developer <89622068+TuanminhDev@users.noreply.github.com>
2021-08-29 21:46:31 +0700

    Create lock.lua

---
+++ PCLocker/lock.lua
@@ -0,0 +1,19 @@
+-- i need you compile this
+-- i am owner
+local locker
+call locker.bat
+running: locker.bat <include lock.lua>
+local function lock
+  if password=12345 then
+    print("Correct password")
+    print("press any key to contiue")
+    if Keyboard.Get_signal = end
+    else then
+    print("wrong pass")
+    print("press any key to contiue")
+    if Keyboard.Get_signal = end
+    permission.root()
+    shutdown.rootshutdown
+    end
+  end
+end
